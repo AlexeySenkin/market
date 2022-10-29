397afa397b7636b04dc930aa72a5dc8f97b406a2
i@alexsenkin.ru <@alexsenkin>
2022-10-29 19:48:06 +0300

    Spring2 market lesson 2

--- src/main/java/ru/senkin/spring/market/controllers/ProductController.java
+++ src/main/java/ru/senkin/spring/market/controllers/ProductController.java
@@ -2,11 +2,15 @@ package ru.senkin.spring.market.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import ru.senkin.spring.market.dto.ProductDto;
 import ru.senkin.spring.market.entities.Product;
+import ru.senkin.spring.market.exceptions.ResourceNotFoundException;
 import ru.senkin.spring.market.services.ProductService;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -16,13 +20,24 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    public List<Product> findAllProducts() {
-        return productService.findAll();
+    public List<ProductDto> findAllProducts() {
+        return productService.findAll().stream().map(product -> new ProductDto(product.getId(), product.getTitle(), product.getPrice())).collect(Collectors.toList());
     }
 
+//    @GetMapping("/{id}")
+//    public ResponseEntity<?> findProductById(@PathVariable Long id) {
+//        Optional<Product> product = productService.findById(id);
+//        if (!product.isPresent()) {
+//            ResponseEntity<AppError> err = new ResponseEntity<>(new AppError(HttpStatus.NOT_FOUND.value(), "Product not found, id:" + id), HttpStatus.NOT_FOUND);
+//            return err;
+//        }
+//        return new ResponseEntity<>(product.get(), HttpStatus.OK);
+//    }
+
     @GetMapping("/{id}")
-    public Product findProductById(@PathVariable Long id) {
-        return productService.findById(id).get();
+    public ProductDto findProductById(@PathVariable Long id) {
+        Product product = productService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product not found, id:" + id));
+        return new ProductDto(product.getId(), product.getTitle(), product.getPrice());
     }
 
     @DeleteMapping("/{id}")
